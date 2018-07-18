093c5ad454c57474a06ee9d52b1c7a491c034ef7
Ryan Martin <ryan.martin@shopify.com>
2018-07-17 21:04:07 -0400

    implement incremental loading

--- ApiClient.js
+++ ApiClient.js
@@ -1,7 +1,6 @@
 var config = require('./config');
 
 export default class ApiClient {
-
 	static get(endpoint, headers) {
 		return fetch(`${config.API_ADDRESS}${endpoint}`, {
 				method: 'GET',
@@ -100,4 +99,22 @@ export default class ApiClient {
 			console.error(err);
 		});;
 	}
+
+	static makeCancelable(promise) {
+	  let hasCanceled_ = false;
+
+	  const wrappedPromise = new Promise((resolve, reject) => {
+	    promise.then(
+	      val => hasCanceled_ ? reject({isCanceled: true}) : resolve(val),
+	      error => hasCanceled_ ? reject({isCanceled: true}) : reject(error)
+	    );
+	  });
+
+	  return {
+	    promise: wrappedPromise,
+	    cancel() {
+	      hasCanceled_ = true;
+	    },
+	  };
+	};
 }
--- views/Feed/FeedStyle.js
+++ views/Feed/FeedStyle.js
@@ -1,5 +1,9 @@
 import { StyleSheet } from "react-native";
 
 export default (styles = StyleSheet.create({
-
+	noMorePosts: {
+		textAlign: 'center',
+		marginTop: 10,
+		marginBottom: 10,
+	}
 }));
