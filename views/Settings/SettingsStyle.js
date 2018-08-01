780fc76f0b5398a6f9ba40a587e69b8544fdc1cf
Neil Brubacher <neil.brubacher@hotmail.com>
2018-08-01 00:47:48 -0400

    Updates settings list

--- views/Settings/SettingsStyle.js
+++ views/Settings/SettingsStyle.js
@@ -1,5 +1,19 @@
 import { StyleSheet } from "react-native";
 
 export default (styles = StyleSheet.create({
-
+  contentContainer: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  itemContainer: {
+    flex: 1,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 20,
+    margin: 0,
+    justifyContent: 'flex-start',
+  },
+  itemText: {
+    fontSize: 16,
+  }
 }));
