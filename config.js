791722acc04ec416bbad3464fbd93164713ca26f
Matthew Pollex <matthew.pollex@gmail.com>
2019-08-29 12:08:11 -0400

    Change auth address based on environment

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
+let authServerAddress = 'https://skybunk-auth-dev.herokuapp.com';
+
+if (process.env.NODE_ENV === 'production') {
+  authServerAddress = 'https://skybunk-auth-production.herokuapp.com';
+} else if (process.env.NODE_ENV === 'staging') {
+  authServerAddress = 'https://skybunk-auth-staging.herokuapp.com';
+}
+
 module.exports = {
-  AUTH_ADDRESS: 'https://skybunk-auth-dev.herokuapp.com',
+  AUTH_ADDRESS: authServerAddress,
   VERSION: '6.1.0'
 }
